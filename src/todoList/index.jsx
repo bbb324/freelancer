09798176ce667a290e97124525a3084ea252c1e8
bbb324 <johnigzie@163.com>
2019-03-27 23:41:54 +0800

    '更新文件夹'

--- src/todoList/index.jsx
+++ src/todoList/index.jsx
@@ -1,1 +1,1 @@
-/** * Created by junxie on 18/5/27. */import React from 'react';import ReactDom from 'react-dom';import App from './component/App';ReactDom.render(<App />,    document.getElementById('root'));
+/** * Created by junxie on 18/5/27. */import React from 'react';import ReactDom from 'react-dom';import App from './component/front-page/App';import Analysis from './component/analysis/Analysis.jsx';ReactDom.render(<Analysis />,    document.getElementById('root'));
