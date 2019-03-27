756e826faee11a9f67b85d6c8d5d9f17a2ece52f
bbb324 <johnigzie@163.com>
2019-03-27 21:03:06 +0800

    'init'

---
+++ src/todoList/index.jsx
@@ -0,0 +1,1 @@
+/** * Created by junxie on 18/5/27. */import React from 'react';import ReactDom from 'react-dom';import App from './component/App';ReactDom.render(<App />,    document.getElementById('root'));
