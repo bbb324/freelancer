7367ef8c78a3ea3602ef271659509153da58e5c0
bbb324 <johnigzie@163.com>
2019-03-31 21:10:17 +0800

    '单页面应用分支'

--- build/effect.js
+++ build/effect.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "50e0713e59a18f6ab936";
+/******/ 	var hotCurrentHash = "f886f7d537129744e916";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
