5990342333a20bc14ae1a37eb3fc94fbc4cf81d2
bbb324 <johnigzie@163.com>
2019-04-03 23:39:31 +0800

    '增加计算公式'

--- build/effect.js
+++ build/effect.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e56ed827d5d7248d4dbb";
+/******/ 	var hotCurrentHash = "704ec8e949677120b326";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
