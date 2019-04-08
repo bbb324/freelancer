16af6162058619cd24b43c3fd435fed73aed35fc
bbb324 <johnigzie@163.com>
2019-04-09 00:42:09 +0800

    '单点purify'

--- build/effect.js
+++ build/effect.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "927efc697c17265550db";
+/******/ 	var hotCurrentHash = "1cb88fc5c712d48243f2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
