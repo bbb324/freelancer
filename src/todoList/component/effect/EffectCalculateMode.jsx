0cfa2ab78b4beb52d0ecc13c4d43181c4a930d45
bbb324 <johnigzie@163.com>
2019-04-08 21:46:27 +0800

    '更新计算公式'

--- src/todoList/component/effect/EffectCalculateMode.jsx
+++ src/todoList/component/effect/EffectCalculateMode.jsx
@@ -266,6 +266,37 @@ class Speed extends React.Component {
     }
 }
 
+// 环空返速
+class Loop extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        }
+    }
+    calculate() {
+        // value 环空返速 = 1.2732 * Math.pow(10, 3) *泵排量 / (Math.pow(井眼直径, 2) - Math.pow(钻具外径, 2))
+        let value = 1.2732 * Math.pow(10, 3) * get(this.refs.a) / (Math.pow(get(this.refs.b), 2) - Math.pow(get(this.refs.c), 2));
+        this.props.setValue(value, this.props.code);
+        this.setState({
+            value: value
+        })
+    }
+    render() {
+        return <div className="math-params">
+            <input placeholder="泵排量" className='cal-input' ref='a'/>
+            <input placeholder="井眼直径" className='cal-input' ref='b'/>
+            <input placeholder="钻具外径" className='cal-input' ref='c'/>
+
+            <div>
+                <span className='cal-btn' onClick={this.calculate.bind(this)}>计算</span>
+                <span> {this.state.value} </span>
+            </div>
+        </div>
+    }
+}
+
+
 module.exports = {
     Power,
     Nozzle,
@@ -274,6 +305,7 @@ module.exports = {
     Pump,
     Drop,
     Jet,
-    Speed
+    Speed,
+    Loop
 
 };
