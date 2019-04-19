2aa27405e2b8f9ef4fa17edc510a1490d8a3d007
bbb324 <johnigzie@163.com>
2019-04-19 22:46:41 +0800

    '钻头水功率计算界面修改'

--- build/purify.js
+++ build/purify.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7ca39ecd6edef0053dbd";
+/******/ 	var hotCurrentHash = "330b5d12b056ee30f882";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/todoList/component/effect/EffectCalculateMode.jsx
+++ src/todoList/component/effect/EffectCalculateMode.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Input from '../common/Input.jsx';
+import FinalCalculate from '../common/FinalCalculate.jsx';
 function get(ref) {
     return +ref.refs[Object.keys(ref.refs)[0]].value
 }
-var event = document.createEvent('HTMLEvents')
+var event = document.createEvent('HTMLEvents');
 
 // 水力功率
 class Power extends React.Component {
@@ -317,6 +318,124 @@ class Loop extends React.Component {
     }
 }
 
+class Total extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: [
+                {label: '泵压', value: ''},
+                {label: '泵排量', value: ''},
+                {label: '钻头直径', value: ''},
+                {label: '钻铤外径', value: ''},
+                {label: '钻杆外径', value: ''},
+                {label: '钻井液密度', value: ''},
+                {label: '井眼直径', value: ''},
+                {label: '喷嘴1', value: ''},
+                {label: '喷嘴2', value: ''},
+                {label: '喷嘴3', value: ''},
+                {label: '喷嘴4', value: ''},
+                {label: '喷嘴5', value: ''},
+                {label: '喷嘴6', value: ''},
+                {label: '喷嘴7', value: ''},
+                {label: '喷嘴8', value: ''},
+            ],
+            output: [
+                {label: '水力功率', value: 0 },
+                {label: '喷嘴压降', value: 0 },
+                {label: '喷嘴水功率', value: 0 },
+                {label: '比水功率', value: 0 },
+                {label: '射流冲击力', value: 0 },
+                {label: '喷射速度', value: 0 },
+                {label: '钻杆环空返速', value: 0 },
+                {label: '钻铤环空返速', value: 0 },
+            ]
+        };
+        this.formula = [
+            '水力功率 = 泵压*泵排量',
+            '喷嘴压降 = 810*钻井液密度*泵排量*泵排量/(0.98*0.98*(喷嘴1*喷嘴1+喷嘴2*喷嘴2+喷嘴3*喷嘴3+喷嘴4*喷嘴4+喷嘴5*喷嘴5+喷嘴6*喷嘴6+喷嘴7*喷嘴7+喷嘴8*喷嘴8)*(喷嘴1*喷嘴1+喷嘴2*喷嘴2+喷嘴3*喷嘴3+喷嘴4*喷嘴4+喷嘴5*喷嘴5+喷嘴6*喷嘴6+喷嘴7*喷嘴7+喷嘴8*喷嘴8)',
+            '喷嘴水功率 = 喷嘴压降*泵排量',
+            '比水功率 = 喷嘴水功率*1000/(π*钻头直径*钻头直径/4)',
+            '射流冲击力 = 1273*钻井液密度*泵排量*泵排量/(喷嘴1*喷嘴1+喷嘴2*喷嘴2+喷嘴3*喷嘴3+喷嘴4*喷嘴4+喷嘴5*喷嘴5+喷嘴6*喷嘴6+喷嘴7*喷嘴7+喷嘴8*喷嘴8)',
+            '喷射速度 = 1273*泵排量/(喷嘴1*喷嘴1+喷嘴2*喷嘴2+喷嘴3*喷嘴3+喷嘴4*喷嘴4+喷嘴5*喷嘴5+喷嘴6*喷嘴6+喷嘴7*喷嘴7+喷嘴8*喷嘴8)',
+            '钻杆环空返速 = 1.2732 * Math.pow(10, 3) *泵排量 / (Math.pow(井眼直径, 2) - Math.pow(钻杆外径, 2))',
+            '钻铤环空返速 = 1.2732 * Math.pow(10, 3) *泵排量 / (Math.pow(井眼直径, 2) - Math.pow(钻铤外径, 2))'
+        ];
+
+    }
+
+    componentWillMount() {
+        let inputs = Object.assign(this.state.input, []);
+        let outputs = Object.assign(this.state.output, []);
+        inputs.forEach(item => {
+            item.value = window.localStorage.getItem(item.label);
+        })
+        outputs.forEach(item => {
+            item.value = window.localStorage.getItem(item.label);
+        });
+        this.setState({
+            input: inputs,
+            output: outputs
+        });
+    }
+
+
+    getValue(label) {
+        let unit = this.totalParams.filter(item => {
+            return item.label === label;
+        });
+        return unit[0].value
+    }
+
+    setValue(inputParams) {
+        this.totalParams = inputParams;
+        const outputs = Object.assign(this.state.output, []);
+
+        // v: 7628 * 塑性粘度^0.2*钻井液密度^0.8 * 泵排量^1.8 * 钻具长度/钻具内径^4.82
+        let v = '';
+
+        // v2: 喷嘴1*喷嘴1+喷嘴2*喷嘴2+喷嘴3*喷嘴3+喷嘴4*喷嘴4+喷嘴5*喷嘴5+喷嘴6*喷嘴6+喷嘴7*喷嘴7+喷嘴8*喷嘴8)*(喷嘴1*喷嘴1+喷嘴2*喷嘴2+喷嘴3*喷嘴3+喷嘴4*喷嘴4+喷嘴5*喷嘴5+喷嘴6*喷嘴6+喷嘴7*喷嘴7+喷嘴8*喷嘴8
+        let v2 = this.getValue('喷嘴1') * this.getValue('喷嘴1') +
+            this.getValue('喷嘴2') * this.getValue('喷嘴2') +
+            this.getValue('喷嘴3') * this.getValue('喷嘴3') +
+            this.getValue('喷嘴4') * this.getValue('喷嘴4') +
+            this.getValue('喷嘴5') * this.getValue('喷嘴5') +
+            this.getValue('喷嘴6') * this.getValue('喷嘴6') +
+            this.getValue('喷嘴7') * this.getValue('喷嘴7') +
+            this.getValue('喷嘴8') * this.getValue('喷嘴8');
+
+        // v3: 喷嘴压降
+        let v3 = 810 * this.getValue('钻井液密度') * this.getValue('泵排量') * this.getValue('泵排量') / (0.98 * 0.98 * v2)
+
+        // v4: 喷嘴水功率
+        let v4 = v3 * this.getValue('泵排量');
+
+
+
+        outputs[0].value = this.getValue('泵压') * this.getValue('泵排量');
+        outputs[1].value = v3;
+        outputs[2].value = v4;
+        outputs[3].value = v4 * 1000 / (Math.PI * this.getValue('钻头直径') * this.getValue('钻头直径')/4);
+        outputs[4].value = 1273 * this.getValue('钻井液密度') * this.getValue('泵排量') * this.getValue('泵排量')/ v2;
+        outputs[5].value = 1273 * this.getValue('泵排量') / v2;
+        outputs[6].value = 1.2732 * Math.pow(10, 3) * this.getValue('泵排量') / (Math.pow(this.getValue('井眼直径'), 2) - Math.pow(this.getValue('钻杆外径'), 2));
+        outputs[7].value = 1.2732 * Math.pow(10, 3) * this.getValue('泵排量') / (Math.pow(this.getValue('井眼直径'), 2) - Math.pow(this.getValue('钻铤外径'), 2));
+
+
+
+        outputs.forEach(item => {
+            window.localStorage.setItem(item.label, item.value);
+        });
+
+        this.setState({
+            output: outputs
+        });
+        this.props.setBack(outputs);
+    }
+    render() {
+        return <FinalCalculate inputParams={this.state.input} outputParams={this.state.output} setValue={this.setValue.bind(this)} title={'环空返速'} formula={this.formula}/>
+    }
+}
+
 
 module.exports = {
     Power,
@@ -327,6 +446,7 @@ module.exports = {
     Drop,
     Jet,
     Speed,
-    Loop
+    Loop,
+    Total
 
 };
