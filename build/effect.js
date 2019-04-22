985d90b237cbb9e9327b59af0107966e2bef1027
bbb324 <johnigzie@163.com>
2019-04-22 22:47:30 +0800

    '更新计算公式'

--- build/effect.js
+++ build/effect.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "244f1b0a652df7e19b52";
+/******/ 	var hotCurrentHash = "19af56b7c51e0e334e69";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
