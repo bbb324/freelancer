3eaa5b804cbea32b82a36580d187e50f7c123826
bbb324 <johnigzie@163.com>
2019-04-22 22:54:31 +0800

    '更新计算公式'

--- build/purify.js
+++ build/purify.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7d1bb62ee9d2f0605469";
+/******/ 	var hotCurrentHash = "ae9490f831170b6128ff";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
