7829a85cdf6e97c3f8a62822301c66ecfc6b24f9
bbb324 <johnigzie@163.com>
2019-04-23 22:12:44 +0800

    '增加localStorage存储'

--- build/effect.js
+++ build/effect.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0bafea2b93d4a47096b8";
+/******/ 	var hotCurrentHash = "fe661d231dd20cf49ee0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
