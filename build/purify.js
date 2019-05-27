520eacd62b63b602c10aced552674db186040c1b
bbb324 <johnigzie@163.com>
2019-05-27 22:08:16 +0800

    调整计算公式

--- build/purify.js
+++ build/purify.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d594aca58874f4e7bcda";
+/******/ 	var hotCurrentHash = "3b67d7ded25b7e7b3704";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
