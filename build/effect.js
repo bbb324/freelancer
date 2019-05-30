220579a2adf01d5154c1e900ca96cab58bbbd59a
谢峻 <jy.xj@alibaba-inc.com>
2019-05-30 17:31:56 +0800

    删除默认值

--- build/effect.js
+++ build/effect.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c005409d836b57f70559";
+/******/ 	var hotCurrentHash = "15a12dcc6d38e1822b97";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
