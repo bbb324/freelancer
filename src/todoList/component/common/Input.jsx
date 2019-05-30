1751bedbf522706a48819e51cfd34eeb26113ced
bbb324 <johnigzie@163.com>
2019-05-30 22:46:10 +0800

    可以自动计算，并且手动输入

--- src/todoList/component/common/Input.jsx
+++ src/todoList/component/common/Input.jsx
@@ -23,12 +23,20 @@ class Input extends React.Component {
         this.props.onChange && this.props.onChange(this.props.code, e.target.value);
     }
 
+    setInput() {
+
+        if(this.props.code === 'p' || this.props.code === 'q') {
+            return <input className='input-value' onChange={this.hasNumber.bind(this)} type="number" placeholder={`请输入${this.props.name}`} ref={this.props.code} value={this.props.defaultValue || ''}/>
+        }
+        return <input className='input-value' onChange={this.hasNumber.bind(this)} type="number" placeholder={`请输入${this.props.name}`} ref={this.props.code} defaultValue={this.props.defaultValue || ''}/>
+
+    }
+
     render() {
         return <div className='input-field-option'>
             <label className='input-label'> {this.props.name}： </label>
             <div className='input-div'>
-                <input className='input-value' onChange={this.hasNumber.bind(this)} type="number" placeholder={`请输入${this.props.name}`} ref={this.props.code} defaultValue={this.props.defaultValue || ''}/>
-
+                {this.setInput()}
             </div>
         </div>
     }
